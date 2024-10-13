234871b2002118af1ee36194325c5146f2f71689
Aryan Kumar <aryankumar.55566@gmail.com>
2024-10-13 21:22:39 +0530

    .

--- CollegeProject/hospital-management-system/hospital-management-system/server.js
+++ CollegeProject/hospital-management-system/hospital-management-system/server.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth'); // Ensure correct path
 
 dotenv.config();  // Load environment variables
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-const authRoutes = require('./routes/auth'); // Ensure correct path
-app.use('/api/auth', authRoutes);
+const PORT = process.env.PORT || 4000;
 
 // Connect to the database
 connectDB();
 
 // Middleware
-app.use(express.json());
+app.use(express.json()); // Middleware to parse JSON bodies
 
+app.get('/', (req, res) => {
+    res.send('Server works!');
+  });
 // Routes
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', authRoutes); // Register auth routes
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
